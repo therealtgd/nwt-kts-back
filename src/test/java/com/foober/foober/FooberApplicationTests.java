42d2d585264978e461969dc3b3929636d668b3b2
therealtgd <vladanmikic2000@gmail.com>
2022-11-15 20:43:30 +0100

    Init Spring project

---
+++ src/test/java/com/foober/foober/FooberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foober.foober;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
