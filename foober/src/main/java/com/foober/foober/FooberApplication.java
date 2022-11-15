2e9cb8c4fc5ee12cba75245d3ebe4d0af0476a4b
therealtgd <vladanmikic2000@gmail.com>
2022-11-15 20:43:30 +0100

    Init Spring project

---
+++ foober/src/main/java/com/foober/foober/FooberApplication.java
@@ -0,0 +1,13 @@
+package com.foober.foober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooberApplication.class, args);
+	}
+
+}
