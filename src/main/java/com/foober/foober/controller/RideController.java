aa7dd02ce59ba0455097dc64d0805bd90e713263
Nikola Damjanovic <iamnikoladamjanovic@gmail.com>
2023-01-30 19:33:18 +0100

    Replace ResponseEntity with ApiResponse

--- src/main/java/com/foober/foober/controller/RideController.java
+++ src/main/java/com/foober/foober/controller/RideController.java
@@ -1,11 +1,14 @@
 package com.foober.foober.controller;
 
+import com.foober.foober.dto.ApiResponse;
 import com.foober.foober.service.RideService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/ride" ,produces = MediaType.APPLICATION_JSON_VALUE)
@@ -15,16 +18,16 @@ public class RideController {
     private final RideService rideService;
 
     @GetMapping("/price")
-    public ResponseEntity<Integer> getPrice(
+    public ApiResponse<Integer> getPrice(
         @RequestParam(value="vehicleType") String vehicleType,
         @RequestParam(value = "distance") int distance
     ) {
         try {
-            return ResponseEntity.ok(rideService.getPrice(vehicleType, distance));
+            return new ApiResponse<>(rideService.getPrice(vehicleType, distance));
         } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ApiResponse<>(HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
