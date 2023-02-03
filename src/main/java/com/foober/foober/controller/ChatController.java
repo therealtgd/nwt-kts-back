4260cd583d1068c5486e1cba88ec9f281f81ea14
Nikola Damjanovic <iamnikoladamjanovic@gmail.com>
2023-02-03 14:08:29 +0100

    Add drive simulation on back

--- src/main/java/com/foober/foober/controller/ChatController.java
+++ src/main/java/com/foober/foober/controller/ChatController.java
@@ -7,7 +7,6 @@ import com.foober.foober.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
